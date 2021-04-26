2b44d4c58d8e1915261bb1047890f83933b92b83
V-SPOT <83084169+ajmalyaseen@users.noreply.github.com>
2021-04-26 17:51:23 +0530

    Update translation.py
    Translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,24 +5,21 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am How are you 🤷‍♂
+<i> How are you 🤷‍♂
 
- 
-
-ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ @Film_Zone_Group ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
+ ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <b><i><a href="https://t.me/Film_zone_group">Film_Zone™</a></i></b>
+ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
 
 🔱 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 @V_spot
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>sorry you can't use me?</u></i></b>
-
-If make me Dm me 👇
-
+<b><i>sorry you can't use me?</i></b>
+<b>If You make me Message for my owner</b> 👇
 @v_spot
 """
     
-    ABOUT_TEXT = """<b>💠 Name</b> : <code> Fz Links </code>
+    ABOUT_TEXT = """<b>💠 My Name</b> : <code> Fz Links </code>
     
 <b>💠 Creator</b> : <b><i><a href="https://t.me/v_spot">v_spot</a></i></b>
 
@@ -30,5 +27,5 @@ If make me Dm me 👇
 
 <b>💠 Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram</a></i>
 
-<b>💠 Source Code</b> : <i><a href="https://t.me/v_spot">Dm me</a></i>
+<b>💠 Source Code</b> : <i>Dm me</i>
 """
