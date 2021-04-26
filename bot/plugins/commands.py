1069e48d495999c420551da6a98ed4eb97000bda
V-SPOT <83084169+ajmalyaseen@users.noreply.github.com>
2021-04-26 21:46:40 +0530

    Commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -121,6 +121,7 @@ async def help(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_TEXT,
+        reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
