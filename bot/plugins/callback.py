c603b48a4ef474388a9ce6d1b70c6159a1e724ca
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-03 08:04:26 +0100

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1640,7 +1640,7 @@ async def callback_data(bot, update: CallbackQuery):
     if query_data == "start":
         buttons = [[
             InlineKeyboardButton('My Dev ⚡', url='https://t.me/MRK_YT'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/MRK-YT/Pro-Auto-Filter-Bot')
+            InlineKeyboardButton('Source Code 🧾', url ='https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMRK-YT%2FUnlimited-Filter-Bot%2Fblob%2Fmain%2FREADME.md&template=https%3A%2F%2Fgithub.com%2FMRK-YT%2FPro-Auto-Filter-Bot.git')
         ],[
             InlineKeyboardButton('Support🛠', url='https://t.me/Mo_Tech_YT'),
             InlineKeyboardButton('Tutorial Video', url='https://t.me/Mo_Tech_YT/26')
