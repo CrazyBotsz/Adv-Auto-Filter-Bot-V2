bbdfe061522a1e9704bc8d18fa14e8828c21a068
Albert Einstein <73480087+AlbertEinsteinTG@users.noreply.github.com>
2021-05-09 15:29:59 +0530

    ......

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -64,11 +64,16 @@ async def auto_filter(bot, update):
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
             file_link = filter.get("file_link")
-            file_size = int(filter.get("file_size", ""))
-            file_size = round(file_size/(1024*1024)) # from B to MiB
-            file_size = str(file_size) + " MiB" if file_size < 1024 else str(round(file_size/1024)) + " GiB"
+            file_size = int(filter.get("file_size", "0"))
+            
+            # from B to MiB
+            file_size = round(file_size/(1024*1024))
+            
+            file_size = f"[{str(file_size)}] MiB " if file_size < 1024 else f"[{str(round(file_size/1024))}] GiB "
+            file_size = "" if file_size == ("[0] MiB" or "[0] GiB") else file_size
+            
                             # add emoji down below inside " " if you want..
-            button_text = f"[{file_size}] {file_name}" if file_size else file_name
+            button_text = f"{file_size}{file_name}" if file_size else file_name
             
             if file_type == "video":
                 if allow_video: 
