bced0236cf7f13542c7800510ecc59abf4006831
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-19 12:19:23 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,8 +19,13 @@ For More Details Click Help Button Below..
 </i>""")
 START_MSG_BUTTON_NAME_1 = os.environ.get("START_MSG_BUTTON_NAME_1", "Developers")
 START_MSG_BUTTIN_NAME_2 = os.environ.get("START_MSG_BUTTON_NAME_2", "Source Code 🧾")
+START_MSG_BUTTON_NAME_3 = os.environ.get("START_MSG_BUTTON_NAME_3", "Support 🛠")
 START_MSG_BUTTON_LINK_1 = os.environ.get("START_MSG_BUTTIN_LINK_1", "https://t.me/CrazyBotsz")
 START_MSG_BUTTON_LINK_2 = os.environ.get("START_MSG_BUTTON_LINK_2", "https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2")
+START_MSG_BUTTON_LINK_3 = os.environ.get("START_MSG_BUTTON_LINK_3", "https://t.me/CrazyBotszGrp")
+FILE_SENT_BUTTON_NAME = os.environ.get("FILE_SENT_BUTTON_NAME", "Developers")
+FILE_SENT_BUTTON_LINK = os.environ.get("FILE_SENT_BUTTON_LINK", "https://t.me/CrazyBotsz")
+
 
 # Change Accordingly While Deploying To A VPS
 APP_ID = int(os.environ.get("APP_ID"))
