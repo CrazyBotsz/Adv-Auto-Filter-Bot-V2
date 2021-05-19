7b4a5918911c2b5cad9375b7c7e68960657c24ca
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-19 13:12:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -136,6 +136,7 @@ async def start(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
+            return
         except:
             pass
 
