2f9bc6a7533c6f52938a669d7f1cd33fbd31fe42
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-19 19:04:28 +0530

    👨‍💻Bug Fix😏

--- bot/plugins/channel.py
+++ bot/plugins/channel.py
@@ -276,7 +276,7 @@ async def delall(bot: Bot, update):
         VERIFY[str(chat_id)] = admin_list
 
     if not user_id in VERIFY.get(str(chat_id)):
-        return
+                return
     
     await db.delete_all(chat_id)
     await recacher(chat_id, True, True, bot, update)
@@ -284,7 +284,7 @@ async def delall(bot: Bot, update):
     await update.reply_text("Sucessfully Deleted All Connected Chats From This Group....")
 
 
-@Client.on_message(filters.channel & (filters.video | filters.audio | filters.document), group=0)
+@Client.on_message(filters.channel & (filters.video | filters.audio | filters.document) & ~filters.edited, group=0)
 async def new_files(bot: Bot, update):
     """
     A Funtion To Handle Incoming New Files In A Channel ANd Add Them To Respective Channels..
