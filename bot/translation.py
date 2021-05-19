b4826c7191555a0750a684b17ac4fbe3b60a76f1
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-19 15:14:23 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @AlbertEinsteinTG & Dark Angel
 
 class Translation(object):
     
@@ -21,19 +21,14 @@ For More Details Click Help Button Below..
 </i>
 <b><u>Bot Commands (Works Only In Groups) :</b></u>
 -> <code>/add chat_id</code>
-            OR  -> Connect Group and Channel
+            <b>OR  -> Connect Group and Channel</b>
 -> <code>/add @Username</code> 
 -> <code>/del chat_id</code>
-            OR  ->  Disconnect Group and Channel    
+            <b>OR  ->  Disconnect Group and Channel</b>    
 -> <code>/del @Username</code>
--> <code>/delall</code>
-    
--> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-    ° <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-    ° <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-    ° <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-    ° <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
+-> <code>/delall</code>  
+-> <code>/settings</code> 
+                <b>->  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
 @CrazyBotsz
 """
     
