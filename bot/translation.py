9a3df1b32cb6adc61a1ea8f0ad2853235004221d
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-19 16:21:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -32,13 +32,10 @@ For More Details Click Help Button Below..
 @CrazyBotsz
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>  
+<b>➥ Creator of Original Repo </b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator of Customisable Repo </b> : <b><i><a href="https://t.me/DarkzzAngel">Dark Angel</a></i></b>
 <b>➥ Language</b> : <code>Python3</code>
-
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
 <b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
