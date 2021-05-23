7637fd9cb00b21bdc292131373a2fa8ee91e00fc
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-23 10:44:34 +0530

    👨‍💻Update...

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            button_text = f"{file_size}«🎬»{file_name}"
             
 
             if file_type == "video":
