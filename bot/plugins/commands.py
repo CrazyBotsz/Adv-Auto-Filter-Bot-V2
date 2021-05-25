f4d0f4968a8ee995af9bada29933153517207d7a
sajjadafnan <71611618+sajjadafnan@users.noreply.github.com>
2021-05-25 11:33:48 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/malayalam_movees"
                                 )
                         ]
                     ]
