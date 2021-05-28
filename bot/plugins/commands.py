9a62c6e70456e9da45e829d89048c9f02648c11b
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-28 12:19:08 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @AlbertEinsteinTG & @MRK_YT
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/Mo_TECH_YT/26"
+                                    '👨‍💼Developers👨‍💼', url="https://t.me/Mo_TECH_YT/26"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/Mo_TECH_YT/26"
+                                    '👨‍💼Developers👨‍💼', url="https://t.me/Mo_TECH_YT/26"
                                 )
                         ]
                     ]
@@ -93,7 +93,7 @@ async def start(bot, update):
         InlineKeyboardButton('Source Code🧾', url ='https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMRK-YT%2FUnlimited-Filter-Bot%2Fblob%2Fmain%2FREADME.md&template=https%3A%2F%2Fgithub.com%2FMRK-YT%2FPro-Auto-Filter-Bot.git')
     ],[
         InlineKeyboardButton('Support🛠', url='https://t.me/Mo_TECH_YT'),
-        InlineKeyboardButton('Tutorial Video', url='https://t.me/Mo_Tech_YT/26')
+        InlineKeyboardButton('Tutorial Video', url='https://youtu.be/uAHl5jvnrhk')
     ],[
         InlineKeyboardButton('⚙Help⚙', callback_data="help")
     ]]
