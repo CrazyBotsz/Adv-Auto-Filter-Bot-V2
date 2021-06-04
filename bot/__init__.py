3f0217703c4c0d434fab3c8ccf95eee80ef4126f
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-04 07:00:06 +0530

    👨‍💻Update Channel

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,6 +10,8 @@ from logging.handlers import RotatingFileHandler
 
 from .translation import Translation
 
+# Update Channel
+UPDATE_CHANNEL = os.environ.get("MT_UPDATE_CHANNEL")
 # Change Accordingly While Deploying To A VPS
 APP_ID = int(os.environ.get("APP_ID"))
 
