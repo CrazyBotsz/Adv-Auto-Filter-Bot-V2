44290b70c00a86f1e6045317cb471921c21eee49
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-04 07:23:23 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG, @Mrk_YT & @DarkzzAngel
+# (c) @AlbertEinsteinTG & @Mrk_YT
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
