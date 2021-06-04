4da863284a9e7132b29c50c278ca2835735fcaef
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-04 17:49:06 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -2,7 +2,30 @@
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG & @Mrk_YT
 
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from bot import Translation # pylint: disable=import-error
+from bot.database import Database # pylint: disable=import-error
 
+db = Database()
+
+@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+async def start(bot, update):
+    
+    try:
+        file_uid = update.command[1]
+    except IndexError:
+        file_uid = False
+    
+    if file_uid:
+        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+        
+        if (file_id or file_type) == None:
+            return
+        
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        
+        if file_type == "document":
         
             await bot.send_document(
                 chat_id=update.chat.id,
