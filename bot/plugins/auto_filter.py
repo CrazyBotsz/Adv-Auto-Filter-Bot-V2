2f6938f0d8afc9b6d47d18b46fd4f23a44b239c9
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-05 16:42:56 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
                         file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            button_text = f"📁 {file_name}"
             
 
             if file_type == "video":
@@ -121,7 +121,8 @@ async def auto_filter(bot, update):
             
             results.append(
                 [
-                    InlineKeyboardButton(button_text, url=file_link)
+                    InlineKeyboardButton(button_text, url=file_link),
+                    InlineKeyboardButton(file_size, callback_data="ignore"), 
                 ]
             )
         
