14fce7a603f0e20488d5bd2b0a85b3334d9cc034
Commanidiot <86559258+Commanidiot@users.noreply.github.com>
2021-06-29 10:23:36 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,58 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @NTG_Family
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance helper  Bot to @NTG_family....😉
+<i>Am Just A Frnd to Movies World....😉
 
-Hi iam helper to movie world @T5links you need to for our biggest family🔥🔥😝
+U Type Movie Name Crtly...❤❤
 
 For More Details Click Help Button Below..
 @NTG_family
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>If Any Issues How to connect?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+👉 Come Admin Or Owner 
+👉 Contact To @NTG_Family
 </i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
+<b>Bot How Be Safe From Illegal Persons:</b>
 
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+    👉 If Any Persons Who Any Illegal Thing Don't Believe Them 
+      
+    👉 If Any Offer  U Got Get Confirmed  To Admin Or Owner 
             
 @NTG_family
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> ℳøvЇᎬꢺ⚔️𝕃𝕆𝕍𝔼ℝ𝕊 ❤
+ </code>
     
 <b>➥Movie world Creator</b> : <b><i><a href="https://t.me/NTG_family">NTG_family</a></i></b>
 
-<b>➥ Language</b> : 🔥Tamilan🔥
+<b>➥ Language</b> : 🤏😎 முரளி 🤏😎
 
-<b>➥  Web series</b> : @T8links
+<b>➥ Movies World</b> : @T5links
 
 """
 
