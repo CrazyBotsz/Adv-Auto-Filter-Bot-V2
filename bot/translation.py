f16625ecadde41ee4f7aa902d0f53c5e6af01596
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-07-12 17:50:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@world_wide_movies
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
