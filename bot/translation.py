c7f9f69e34e3a0f999db106db102187f29ba66cd
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2021-08-22 12:25:32 +0530

    Update translation.py
    Spelling mistakes

--- bot/translation.py
+++ bot/translation.py
@@ -7,7 +7,7 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me To Your Group And Channel And Connect Them And See My Powers 🔥🔥😝
 
 For More Details Click Help Button Below..
 @CrazyBotsz
