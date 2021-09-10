2bde572476ee8486ae16ea4162d7a2f96d29ae9e
MrAnonymous071 <88572086+MrAnonymous071@users.noreply.github.com>
2021-09-10 10:10:55 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,13 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Am Just A Advance Auto Filter Bot..😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Works only for @Bucket_LinkZz ❤️ its order from my boss @Jana_Tg .😇
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+
+@Bucket_LinkZz
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +49,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Jana_TG">Jana_TG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://youtu.be/xrO6ZzN5mlg">Don't Click here</a></i>
 """
