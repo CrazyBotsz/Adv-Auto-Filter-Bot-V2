bc4a4ab786046558de6eeac1bdcf58bedd9fd5f2
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-05 12:58:06 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,14 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b> Oi {}!!</b>
+<i>Enne Pole Vere Arum Illa.Njan Superman Autofilter botinte Coding Ulla Batman 😁
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
+Chumma Add Cheyyth Nokk.Enik Jeevan Thannath @saminsumesh Noobie Ahn🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@housefull_links
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +44,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Housefull_links
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/housefull_links">Housefull_links</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/">Click Me</a></i>
 """
