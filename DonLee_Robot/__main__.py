3288fd7ed13d28d72b75bbe05847b0696ccbdd8c
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-10-09 11:52:44 +0530

    💡

--- DonLee_Robot/__main__.py
+++ DonLee_Robot/__main__.py
@@ -6,6 +6,6 @@
 # All rights reserved by PR0FESS0R-99
 # License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
 
-from .donlee_robot import Donlee_Robot
-app = Donlee_Robot()
+from .donlee_robot import DonLee_Robot
+app = DonLee_Robot()
 app.run()
