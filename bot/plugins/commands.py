19ba64b4352d764676150515977818738b932762
RAJANGAMR <85456269+RAJANGAMR@users.noreply.github.com>
2021-10-19 16:57:06 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/PXMEDIA_RAJANGAM"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/PXMEDIA_RAJANGAM'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/RAJANGAMR/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/aedaham_library_noolakam')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
