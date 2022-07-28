4a42d627671a0e0af6d1bc86f6ad6145653e5ccb
scrooge001 <72791356+scrooge001@users.noreply.github.com>
2022-07-28 19:55:04 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,13 +13,13 @@ from .translation import Translation
 # Change Accordingly While Deploying To A VPS
 APP_ID = int(os.environ.get("APP_ID"6766195))
 
-API_HASH = os.environ.get("95900e1df408a32d044c1939a2ba4fd9")
+API_HASH = os.environ.get("API_HASH"95900e1df408a32d044c1939a2ba4fd9)
 
-BOT_TOKEN = os.environ.get("5498939115:AAHT8YDsRcL9SDaOYOjufOOAQLGJ_jX6Kw4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN"5498939115:AAHT8YDsRcL9SDaOYOjufOOAQLGJ_jX6Kw4)
 
-DB_URI = os.environ.get("mongodb+srv://perera51:perera51@cluster0.xhzyav3.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DB_URI"mongodb+srv://perera51:perera51@cluster0.xhzyav3.mongodb.net/?retryWrites=true&w=majority)
 
-USER_SESSION = os.environ.get("BQAxplszMOj3yZ3iGdI43K_nZT7ww7LuXvRpOhBjFPU-iNP45lVCd2evQ6CxaaO2HHMRmCBHgxNCDpJjD5qjTMJKzaO80WpH4pMVwdLs6Cdfs3iO1bjQRFwXUz5W14BG0FCvx3nuhqr7R8oRSuh7SEp6x3m-NdFz-kX5aM_oCIXXJtoQlEn2ad6bFAbKBe9cko0FR83hKpG24BDKa7sv7JSQyeS5-PDdnYgpGQYMdf_4Wl8Ue-SAtIFZ_Q6GQ36b-zU46E5rCB76F82Sh2y2_Ik7Q8wlt8Jo1lfAlE9VDkizmqfKFuiUSycQWZFElXrRMrLvoPnKrzCwC8Ract0hCPwYAAAAAG5wiOcA")
+USER_SESSION = os.environ.get("USER_SESSION"BQAxplszMOj3yZ3iGdI43K_nZT7ww7LuXvRpOhBjFPU-iNP45lVCd2evQ6CxaaO2HHMRmCBHgxNCDpJjD5qjTMJKzaO80WpH4pMVwdLs6Cdfs3iO1bjQRFwXUz5W14BG0FCvx3nuhqr7R8oRSuh7SEp6x3m-NdFz-kX5aM_oCIXXJtoQlEn2ad6bFAbKBe9cko0FR83hKpG24BDKa7sv7JSQyeS5-PDdnYgpGQYMdf_4Wl8Ue-SAtIFZ_Q6GQ36b-zU46E5rCB76F82Sh2y2_Ik7Q8wlt8Jo1lfAlE9VDkizmqfKFuiUSycQWZFElXrRMrLvoPnKrzCwC8Ract0hCPwYAAAAAG5wiOcA)
 
 VERIFY = {}
 
