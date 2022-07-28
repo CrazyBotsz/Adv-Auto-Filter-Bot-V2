6091f88f5565a827c112aca5ab4c49fbe7bd13f9
scrooge001 <72791356+scrooge001@users.noreply.github.com>
2022-07-28 20:07:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/scrooge010'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/scrooge010')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/scrooge010')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
